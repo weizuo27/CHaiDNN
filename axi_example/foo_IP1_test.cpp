8120267086c14cdac5c2fd6b09fd1d9b6b8bf74e
Wei Zuo <zuowei880227@gmail.com>
2018-11-08 15:20:11 -0600

    added the axi example

---
+++ axi_example/foo_IP1_test.cpp
@@ -0,0 +1,54 @@
+#include "/opt/Xilinx/Vivado/2018.2/include/gmp.h"
+#include "head.hpp"
+#include "stdlib.h"
+#include "stdio.h"
+int main(){
+	printf("just started\n");
+	uint in_main[N*N];
+	AXI_STREAM out;
+	uint out_main[(N-2)*N];
+	uint out_gold[(N-2)*N];
+
+	int i, j;
+	printf("before initialization\n");
+
+	//input initialization
+	for(i = 0; i < N; i++){
+		for(j = 0; j < N; j++){
+			in_main[i*N+j] = i;
+		}
+	}
+	printf("after initialization\n");
+
+	//Create reference output
+	for(i = 0; i <N-2; i++){
+		for(j = 0; j<N; j++){
+			out_gold[i*N + j] = in_main[i*N+j]+in_main[(i+1)*N+j]+in_main[(i+2)*N+j];
+		}
+	}
+
+	int dest_out = 1; //Not useful for one IP simulation
+
+	printf("before launching IP\n");
+
+	foo_IP1 (in_main, out, &dest_out);
+	printf("after launching IP\n");
+
+	//Unpack the output
+	for(i = 0; i < N-2; i++)
+		for(j = 0; j < N; j++){
+			out_main[i*N + j] = out.read().data;
+		}
+
+	//Compare the output
+	for(i = 0; i < N-2; i++)
+		for(j =0; j< N; j++){
+			if(out_main[i*N+j] != out_gold[i*N+j]){
+				printf("Incorrect result at [%d][%d], golden is %d, mine is%d\n",
+						i, j, out_gold[i*N+j], out_main[i*N+j]);
+				return -1;
+			}
+		}
+
+		return 0;
+}
---
+++ axi_example/head.hpp
@@ -0,0 +1,13 @@
+#ifndef __HEAD_H__
+#define __HEAD_H__
+#include "/opt/Xilinx/Vivado/2018.2/include/gmp.h"
+#include <ap_int.h>
+#include <ap_axi_sdata.h>
+#include <hls_stream.h>
+#define N 32
+
+typedef ap_axiu<32,4,5,5> AXI_VALUE;
+typedef hls::stream<AXI_VALUE> AXI_STREAM;
+
+void foo_IP1 ( uint *in, AXI_STREAM &out, int *dest_out);
+#endif
