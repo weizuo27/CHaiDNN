71c49efbdd6fd27a744cd34b6433d332972e3aae
Wei Zuo <zuowei880227@gmail.com>
2018-11-08 18:41:35 -0600

    added the axi example, IP3

---
+++ axi_example/IP3/head_ip3.hpp
@@ -0,0 +1,14 @@
+#ifndef __HEAD_H__
+#define __HEAD_H__
+#include "/opt/Xilinx/Vivado/2018.2/include/gmp.h"
+#include <ap_int.h>
+#include <ap_axi_sdata.h>
+#include <hls_stream.h>
+#define N 32
+#define M 28
+
+typedef ap_axiu<32,4,5,5> AXI_VALUE;
+typedef hls::stream<AXI_VALUE > AXI_STREAM;
+
+void foo_IP3 ( AXI_STREAM &in, uint* out);
+#endif
